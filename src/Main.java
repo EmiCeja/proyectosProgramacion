634eca51dba5f3b9a97073144f9e6b9f9c3fd38d
EmiCeja <l24121357@morelia.tecnm.mx>
2024-09-11 23:46:44 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+// Sección 1: Información de un libro
+
+        System.out.println("Inteligencia Asertiva");
+        System.out.println("Javiera de la plaza");
+        System.out.println("2007");
+        System.out.println("Sentimientos");
+        System.out.println("263 paginas");
+
+        // Sección 2: Información de un país que deseo visitar
+
+        String NombreDelPais = "Argentina";
+
+        String Capital = "Buenos Aires";
+
+        String IdiomaOficial = "Español";
+
+        int Poblacion = 46234830;
+
+        String moneda = "peso argentino";
+
+        System.out.println("Nombre del pais: " + NombreDelPais);
+        System.out.println("Capital del pais: " + Capital);
+        System.out.println("Idioma del pais: " + IdiomaOficial);
+        System.out.println("Poblacion: " + Poblacion);
+        System.out.println("moneda: " + moneda);
+
+        // Sección 3: Información de un personaje historico favorito
+
+        String NombreCompleto = "José Rómulo Sosa Ortiz";
+
+        String FechaDeNacimiento = "17 de febrero de 1948";
+
+        String Nacionalidad = "Mexicano";
+
+        String Ocupacion = "Cantante";
+
+        String LogroDestacado = "Exito mundial";
+
+        System.out.println("Nombre Completo =" + NombreCompleto);
+        System.out.println("Fecha de Nacimiento =" + FechaDeNacimiento);
+        System.out.println("Nacionalidad =" + Nacionalidad);
+        System.out.println("Ocupacion =" + Ocupacion);
+        System.out.println("LogroDestacado =" + LogroDestacado);
+
+    }
+}
